eda683df77c0be6c684b925e95e38d94e025cb4a
masadpc123-oss <masadpc123@gmail.com>
2026-05-03 00:28:10 +0500

    Added Q1 even-odd file

---
+++ Asad_task/Q 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter Number: "<<endl;
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<a<<" Even Number ";
+	}else
+	{
+		cout<<a<<" Odd Number: ";
+	}
+	
+	return 0;
+}
