51fcb44b32e17d3e7624f1e5cbc2454f8f14815e
masadpc123-oss <masadpc123@gmail.com>
2026-05-05 20:16:15 +0500

    Asad_week_2_Tuesday

---
+++ Asad_task/Asad_week_2_Tuesday.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	char name[50];
+	cout<<"Enter string: ";
+	cin>>name;
+	
+	//string Length
+	int str_length=strlen(name);
+	cout<<"String Length = "<<str_length;
+	char copy[100];
+	
+	strcpy(copy, name); //strcpy Function
+	cout<<endl<<"Name: "<<copy;
+	
+	
+	//String Reversal
+	char rev[50];
+	for(int i=0; i<str_length; i++)
+	{
+		rev[i]=name[str_length-i-1];
+	}
+	
+	rev[str_length]='\0'; //string end karne keliye null use kia hai
+	cout<<endl<<"Reverse String: "<<rev; 
+	cout<<endl;
+	
+	
+	//Check palindrome
+	if(strcmp(name, rev)==0) //this func is used to compare 2 string
+	{
+		cout<<name<<" is Palindrome:";
+	}else
+	{
+		cout<<name<<" is not Palindrome:";
+	}
+}
