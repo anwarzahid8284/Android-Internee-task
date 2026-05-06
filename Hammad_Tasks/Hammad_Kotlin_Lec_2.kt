f67e53c8406be480936f11a61b68719c60019e8f
Ahmad_Coder <ahmad8627412@gmail.com>
2026-05-06 09:17:44 -0700

    Hammad_Kotlin_Lec2

---
+++ Hammad_Tasks/Hammad_Kotlin_Lec_2.kt
@@ -0,0 +1,43 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   // Variable Deffine the specific box of memory Name just like Room 1 etc
+
+    // Types of Variables
+
+    // var --> It can bi Re-assigned
+    // val --> It cont be Re-assigned
+
+    var name="Hammad"  // -> type inferencing mean kotlin khood type deta he
+
+    println(name)  // println -> for next line
+
+    // var can be Re_Assigned
+
+    name="Ahmad"
+
+    println(name)
+
+    val marks=4005 // --> val cant be Re_assigned
+
+    // -> marks =600 Show Error Dueto Val
+    println(marks)
+
+    // Explicite Data type deeena
+
+    var f_Name : String ="Taj M"
+
+    println(f_Name)
+
+    val Roll : Double = 2.09
+
+    // --> Roll Cant be Re_assignerd Dueto Val
+
+    println(Roll)
+
+
+
+
+    
+
+}
