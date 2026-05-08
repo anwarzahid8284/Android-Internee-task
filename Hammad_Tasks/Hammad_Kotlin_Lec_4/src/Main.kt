325ba6748ac2727d8d28e69c223f644c6a00e10c
Ahmad_Coder <ahmad8627412@gmail.com>
2026-05-08 10:46:53 -0700

    Hammad_Kotlin_Lec_4

---
+++ Hammad_Tasks/Hammad_Kotlin_Lec_4/src/Main.kt
@@ -0,0 +1,41 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    // In this we will says about the three logical operators
+
+    // && , || and !
+
+    // use of &&
+
+    val above70=false
+    val knowPrograming=false
+
+    val calledForInterview=above70 && knowPrograming
+
+    // result will false
+
+    println(calledForInterview)
+
+// use of ||
+
+    val A=true
+
+    val B=false
+
+    val result =A||B
+
+    // Result will be true becauser of || operator
+
+    println(result)
+
+    // Use of !
+
+    val X=true
+
+    val outPut= !X
+
+    // Due to ! it Olet the value and give now it false
+
+    println(outPut)
+
+}
