7b6d86701394011a21e45e5ee5945cbd279821d4
Ahmad_Coder <ahmad8627412@gmail.com>
2026-05-09 01:21:38 -0700

    Hammad_Kotlin_Lec_7

---
+++ Hammad_Tasks/Hammad_Kotlin_Lec_7/src/Main.kt
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+  // While loop
+
+    var index=5
+
+    while(index>=1){
+        println("Hello ")
+        index--
+    }
+
+    // while loop table of numbers
+
+    var n=2
+    var ind=1
+    while(ind<=10){
+        println(ind*n)
+        ind++
+    }
+    println("The outSide of loop iddex value is " +ind)
+
+    // Do while loop
+
+    var num=5
+
+    do{
+        println("i am hammad")
+    }while(num>8)
+
+}
