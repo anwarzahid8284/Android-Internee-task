9d8e4554507c3fd392d6d430d2eb2eae19d5f73e
masadpc123-oss <masadpc123@gmail.com>
2026-05-09 15:35:13 +0500

    asad_kotlin_lec_2

---
+++ Asad_task/asad_kotlin_lec_2.kt
@@ -0,0 +1,23 @@
+fun main()
+{
+    var a=12;
+    println(a);
+    a=10;  //var reassig ho sakta hai
+    print("A= ")
+    println(a);
+
+    val name="Ali"; //but val ko dubara reassign nhin ki ja sakta
+    println(name);
+   var score : Int=-12; //ye only integer value ko store karta hai
+   var temp: Double=40.5;
+   var isRaining:Boolean=false;
+   var alphabat: Char='B'
+   var name_2: String="M_Asad";
+    println("_____________")
+    println(score)
+    println(temp)
+    println(isRaining)
+    println(alphabat)
+    println(name_2)
+
+}
