6b91b7bcfadaa66c54bb89a65d21c75655e779c3
Aamir khan <iaamirkhan4@gmail.com>
2026-05-12 19:54:15 +0500

    Add files via upload

---
+++ aamiirrrr_task/Aamir_Week1_MondayTask1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, sum = 0;
+    cout << "enter numb ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        sum = sum + i;
+    }
+    cout << "sum = " << sum;
+    return 0;
+}
