47deec213332fe5b8f9bb7112273b4d871c0613a
masadpc123-oss <masadpc123@gmail.com>
2026-05-16 00:58:58 +0500

    asad kotlin file

---
+++ Asad_task/asad_kotlin_lec_7.kt
@@ -0,0 +1,29 @@
+fun main()
+{
+    //Exp: 1
+    var count=4
+    while (count >= 1)
+    {
+        println("Allah");
+        count--;
+    }
+    println()
+
+    //2 table
+    println("2 table: ")
+    var table=2
+   var i=1
+   while (i<=10)
+   {
+       println("$i * $table = ${i * table}")
+       i++
+   }
+
+    //Do while
+    println()
+    println("Do while:")
+    var index=5
+    do {
+        println("Hello")
+    }while (index>5)
+}
