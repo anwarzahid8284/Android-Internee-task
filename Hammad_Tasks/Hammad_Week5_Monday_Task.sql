6b8845ec233ea75a49eb30395decf91257cd8ac4
Ahmad_Coder <ahmad8627412@gmail.com>
2026-06-22 12:52:48 -0700

    Hammad_Week5_Monday_Task

---
+++ Hammad_Tasks/Hammad_Week5_Monday_Task.sql
@@ -0,0 +1,10 @@
+CREATE TABLE school (
+id INT PRIMARY KEY ,
+  name VARCHAR(50),
+  Loc VARCHAR(50) 
+);
+INSERT INTO school VALUES (101,'Al_Fajar','peshawar');
+INSERT INTO school VALUES (102,'Learning','Islamabad');
+INSERT INTO school VALUES (103,'To_Learn','Nowshara');
+
+SELECT * FROM school;
